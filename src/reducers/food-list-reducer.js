df0abe866349f07d7ac0b4bd8d7091a88e0fec6d
Mike Manchee <mikemanchee@gmail.com>
2020-12-17 13:04:37 -0800

    add error message test - passed

--- src/reducers/food-list-reducer.js
+++ src/reducers/food-list-reducer.js
@@ -17,6 +17,11 @@ export default (state = initialState, action) => {
         isLoading: false,
         masterFoodList: action.masterFoodList
       });
+    case c. GET_FOODS_FAILURE:
+      return Object.assign({}, state, {
+        isLoading: false,
+        error: action.error
+      })
     default:
       return state;
     }
